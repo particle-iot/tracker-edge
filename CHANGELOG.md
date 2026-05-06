83c04ab843ea9ccfc36c2e3758a341f867d31238
Technobly <technobly@gmail.com>
2026-05-06 17:17:30 -0500

    Bump firmware version to 22

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+## v22
+
+### COMPATIBILTY
+
+Must be built using device OS v3.2.0 or greater.
+
+### ENHANCEMENTS
+
+- Source can be built for device OS >= 6.4.1
+
 ## v21
 
 ### COMPATIBILTY
--- src/tracker_config.h
+++ src/tracker_config.h
@@ -46,7 +46,7 @@
 #endif
 
 #ifndef TRACKER_PRODUCT_VERSION
-#define TRACKER_PRODUCT_VERSION               (21)
+#define TRACKER_PRODUCT_VERSION               (22)
 #endif
 
 #if ( SYSTEM_VERSION >= SYSTEM_VERSION_DEFAULT(4, 0, 0) )
